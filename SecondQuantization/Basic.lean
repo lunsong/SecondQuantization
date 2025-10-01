65def7fb81f5aa6c06b8edb6dc309d90c854d20f
lsong <hustsonglun@gmail.com>
2025-10-01 03:40:26 +0000

    working

--- SecondQuantization/Basic.lean
+++ SecondQuantization/Basic.lean
@@ -14,23 +14,22 @@ inductive CreAnn (α : Type) : Type
   | cre : α → CreAnn α
   | ann : α → CreAnn α
 
-def cre' {α : Type} (x : α) : FreeAlgebra ℝ (CreAnn α) := FreeAlgebra.ι ℝ (CreAnn.cre x)
-def ann' {α : Type} (x : α) : FreeAlgebra ℝ (CreAnn α) := FreeAlgebra.ι ℝ (CreAnn.ann x)
+abbrev cre' {α : Type} (x : α) : FreeAlgebra ℝ (CreAnn α) := FreeAlgebra.ι ℝ (CreAnn.cre x)
+abbrev ann' {α : Type} (x : α) : FreeAlgebra ℝ (CreAnn α) := FreeAlgebra.ι ℝ (CreAnn.ann x)
 
 notation (name := anticommute) "[" a "," b "]ₐ" => a * b + b * a
 
-def commutators (α : Type) : Set (FreeAlgebra ℝ (CreAnn α)) := fun x =>
+abbrev commutators (α : Type) : Set (FreeAlgebra ℝ (CreAnn α)) := fun x =>
   ( ∃ a b, x = [cre' a, cre' b]ₐ ∨ x = [ann' a, ann' b]ₐ ∨ ( a ≠ b ∧ x = [ann' a, cre' b]ₐ)) ∨
   ( ∃ a, x = [ann' a, cre' a]ₐ - 1)
 
 abbrev Operator (α : Type) : Type :=
   (FreeAlgebra ℝ (CreAnn α)) ⧸ (TwoSidedIdeal.span (commutators α)).asIdeal
 
-def cre {α : Type} (x : α) : Operator α := Ideal.Quotient.mk _ (cre' x)
-def ann {α : Type} (x : α) : Operator α := Ideal.Quotient.mk _ (ann' x)
+abbrev cre {α : Type} (x : α) : Operator α := Ideal.Quotient.mk _ (cre' x)
+abbrev ann {α : Type} (x : α) : Operator α := Ideal.Quotient.mk _ (ann' x)
 
 theorem cre_cre {α : Type} (x y : α) : cre x * cre y = -(cre y * cre x) := by
-  unfold cre
   apply eq_neg_iff_add_eq_zero.mpr
   rw[←RingHom.map_mul,←RingHom.map_mul,←RingHom.map_add,←(Ideal.Quotient.mk _).map_zero,
     Ideal.Quotient.mk_eq_mk_iff_sub_mem, sub_zero]
@@ -40,7 +39,6 @@ theorem cre_cre {α : Type} (x y : α) : cre x * cre y = -(cre y * cre x) := by
   left; rfl
 
 theorem ann_ann {α : Type} (x y : α) : ann x * ann y = -(ann y * ann x) := by
-  unfold ann
   apply eq_neg_iff_add_eq_zero.mpr
   rw[←RingHom.map_mul,←RingHom.map_mul,←RingHom.map_add,←(Ideal.Quotient.mk _).map_zero,
     Ideal.Quotient.mk_eq_mk_iff_sub_mem, sub_zero]
@@ -51,7 +49,6 @@ theorem ann_ann {α : Type} (x y : α) : ann x * ann y = -(ann y * ann x) := by
 
 theorem ann_cre_ne {α : Type} (x y : α) : x ≠ y → ann x * cre y = -(cre y * ann x) := by
   intro h
-  unfold ann cre
   apply eq_neg_iff_add_eq_zero.mpr
   rw[←RingHom.map_mul,←RingHom.map_mul,←RingHom.map_add, ←(Ideal.Quotient.mk _).map_zero,
     Ideal.Quotient.mk_eq_mk_iff_sub_mem, sub_zero]
@@ -61,14 +58,12 @@ theorem ann_cre_ne {α : Type} (x y : α) : x ≠ y → ann x * cre y = -(cre y 
   right; right; exact ⟨h, rfl⟩
 
 theorem ann_cre {α : Type} (x : α) :  ann x * cre x = 1 - (cre x * ann x) := by
-  unfold ann cre
   apply eq_sub_of_add_eq
   apply eq_of_sub_eq_zero
   rw[←RingHom.map_mul,←RingHom.map_mul,←RingHom.map_add,←(Ideal.Quotient.mk _).map_zero,
     ←(Ideal.Quotient.mk _).map_one,sub_eq_add_neg,←RingHom.map_neg,←RingHom.map_add,
     Ideal.Quotient.mk_eq_mk_iff_sub_mem, sub_zero,←sub_eq_add_neg]
   apply TwoSidedIdeal.subset_span
-  unfold commutators
   right
   use x
 
@@ -82,40 +77,46 @@ lemma involutive {α : Type} : ∀ x : CreAnn α, conj₀ (conj₀ x) = x
   | CreAnn.cre x => by simp only [conj₀]
   | CreAnn.ann x => by simp only [conj₀]
 
-def conjₐ {α : Type} : (FreeAlgebra ℝ (CreAnn α)) →ₐ[ℝ]  (Operator α)ᵐᵒᵖ :=
-  (Ideal.Quotient.mkₐ ℝ _).comp (FreeAlgebra.lift ℝ (FreeAlgebra.ι ℝ ∘ conj₀)) 
+def conj₁ {α : Type} : (FreeAlgebra ℝ (CreAnn α)) →ₐ[ℝ] (FreeAlgebra ℝ (CreAnn α))ᵐᵒᵖ :=
+  FreeAlgebra.lift ℝ (MulOpposite.op ∘ FreeAlgebra.ι ℝ ∘ conj₀)
 
-def conj {α : Type} : Operator α →ₐ[ℝ] Operator α :=
-  Ideal.Quotient.liftₐ _ conjₐ (by
-    intro a h
-    rw[TwoSidedIdeal.mem_asIdeal] at h
-    refine TwoSidedIdeal.span_induction ?_ ?_ ?_ ?_ ?_ ?_ h
+def conj₂ {α : Type} : (FreeAlgebra ℝ (CreAnn α)) → Operator α :=
+  Ideal.Quotient.mk _ ∘ MulOpposite.unop ∘ conj₁
+
+def conj {α : Type} : Operator α → Operator α :=
+  Quotient.lift conj₂ (by
+    intro a b h
+    rw[←Quotient.eq_iff_equiv] at h
+    have : (Ideal.Quotient.mk _ a : Operator α) = Ideal.Quotient.mk _ b := by
+      simpa [←Ideal.Quotient.mk_eq_mk, Submodule.Quotient.mk]
+    simp only [conj₂,Function.comp_apply,Ideal.Quotient.mk_eq_mk_iff_sub_mem] at this ⊢
+    rw [←MulOpposite.unop_sub, ←map_sub]
+    rw [TwoSidedIdeal.mem_asIdeal] at this
+    generalize a - b = x at this ⊢
+    refine TwoSidedIdeal.span_induction ?_ ?_ ?_ ?_ ?_ ?_ this
     · intro x h
+      rw [TwoSidedIdeal.mem_asIdeal]
+      apply TwoSidedIdeal.subset_span
       rcases h with h | h
-      · rw[conjₐ,AlgHom.comp_apply,Ideal.Quotient.mkₐ_eq_mk,Ideal.Quotient.eq_zero_iff_mem,
-           TwoSidedIdeal.mem_asIdeal]
-        apply TwoSidedIdeal.subset_span
-        rcases h with ⟨a,b,h | h | h⟩
-        · simp [h, cre', conj₀]
-          rw[←ann',←ann']
-          left
-          use a,b
-          simp
-        · simp[h,ann',conj₀]
-          rw[←cre',←cre']
-          left
-          use a,b
-          simp
-        · simp[h.2,ann',cre',conj₀]
-          rw[←ann',←cre']
-          left
-          use b,a
-          right; right
-          nth_rw 1 [add_comm]
-          exact ⟨h.1 ∘ Eq.symm, rfl⟩
-      · rw[conj
+      · rcases h with ⟨a, b, h | h | h⟩
+        · simp[h,conj₁,conj₀]; left; use b,a; right; left; rfl
+        · simp[h,conj₁,conj₀]; left; use b,a; left; rfl
+        · simp[h.2,conj₁,conj₀]; left; use b,a; right; right; exact ⟨h.1.symm, rfl⟩
+      obtain ⟨a, h⟩ := h
+      simp[h, conj₁, conj₀]; right; use a
+    · simp only [map_zero, MulOpposite.unop_zero, zero_mem]
+    · intro x y _ _ h₁ h₂
+      rw[map_add,MulOpposite.unop_add]
+      exact Ideal.add_mem _ h₁ h₂
+    · intros
+      rwa[map_neg,MulOpposite.unop_neg,Ideal.neg_mem_iff]
+    · intro x y _ h
+      rw[map_mul,MulOpposite.unop_mul]
+      exact TwoSidedIdeal.mul_mem_right _ _ _ h
+    · intro x y _ h
+      rw[map_mul,MulOpposite.unop_mul]
+      exact TwoSidedIdeal.mul_mem_left _ _ _ h
   )
-
 
 
 instance operatorStarRing (α : Type) : StarRing (Operator α) where
